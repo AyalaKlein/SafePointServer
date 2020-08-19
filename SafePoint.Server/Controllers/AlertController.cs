0cd604f065f18ad8cf4ffb55d0e507826510ea0f
Gil Mishal <gilmishal@gmail.com>
2020-08-19 20:50:58 +0300

    Added the ability to send alerts

---
+++ SafePoint.Server/Controllers/AlertController.cs
@@ -0,0 +1,30 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafePoint.Server.Controllers
+{
+    public class AlertController : ControllerBase
+    {
+        [HttpPost("alert")]
+        public async Task<ActionResult> GenerateAnAlert(double locX, double locY, double meterRadius)
+        {
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>
+                {
+                    ["locX"] = locX.ToString(),
+                    ["locY"] = locY.ToString(),
+                    ["meterRadius"] = meterRadius.ToString()
+                },
+                Topic = "israel-alerts"
+            };
+            await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            return Ok();
+        }
+    }
+}
+ 
